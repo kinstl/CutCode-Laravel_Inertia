d5380cfbec821eeba2ffed0c0a27b80f9085ddfa
Konstantin Krivilev <inferno750@mail.ru>
2024-09-10 22:36:57 +0300

    init

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>{{ title }}</h1>
+</template>
+
+<script setup>
+import { toRefs } from "vue";
+
+const props = defineProps({
+    title: String,
+});
+
+const { title } = toRefs(props);
+</script>

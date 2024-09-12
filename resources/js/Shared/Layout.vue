6c812da19a784f51eb815c2385cc79ba659d3ead
Konstantin Krivilev <inferno750@mail.ru>
2024-09-12 23:11:00 +0300

    add Layout, add Link, add Head

---
+++ resources/js/Shared/Layout.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container mx-auto">
+        <ul>
+            <li>
+                <Link :href="route('home')">
+                Home
+                </Link>
+            </li>
+            <li>
+                <Link :href="route('about')">
+                About
+                </Link>
+            </li>
+        </ul>
+        <slot />
+    </div>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3';
+</script>
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [IndexController::class, 'index'])->name('home');
+Route::get('/about', [IndexController::class, 'about'])->name('about');
